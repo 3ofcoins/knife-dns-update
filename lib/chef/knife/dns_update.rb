3391d6b788ea26dd03c0afea5b8f35f6e39dc6e8
Maciej Pasternacki <maciej@3ofcoins.net>
2015-11-18 13:52:14 +0100

    Correctly handle zones with more than 100 records

--- lib/chef/knife/dns_update.rb
+++ lib/chef/knife/dns_update.rb
@@ -96,7 +96,7 @@ module KnifeDnsUpdate
 
       managed = Set.new
 
-      zone.records.each do |rec|
+      zone.records.all!.each do |rec|
         next if %w(NS SOA).include? rec.type
         ui.info("Looking at: #{rec.name} IN #{rec.type} #{rec.value}") if config[:verbosity] >= 3
 
